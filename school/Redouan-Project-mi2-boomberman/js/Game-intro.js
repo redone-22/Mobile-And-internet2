ba0ec0da7767cd1affe2fc00e3d2e561a4e81552
redone-22 <redouan.kanaa@student.odisee.be>
2016-04-21 14:56:09 +0200

    Delete Game-intro.js

--- school/Redouan-Project-mi2-boomberman/js/Game-intro.js
+++
@@ -1,5 +0,0 @@
-"use strict";
-/*global $ */
-$(document).ready(function () { $("body").fadeIn(3000);
-                               $("img").click(function () { $("img").fadeOut(3000); });
-                              });
